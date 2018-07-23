73d94e32e79d4b479c8d56bc9ea693c13ea31d9a
JackYangFM <yangxiansen_lion@sina.com>
2018-07-23 23:20:55 +0800

    20180723

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,6 @@
     font-family: 'yofont';
     src:
       url('./assets/iconfonts/iconfont.woff') format('woff'),
-      url('./assets/iconfonts/iconfont.ttf') format('truetype');
+      url('./assets/iconfonts/iconfont.ttf') format('truetype')
   }
 </style>
--- src/components/news/News.vue
+++ src/components/news/News.vue
@@ -2,7 +2,7 @@
 	<div>
 		<header>
 			<span>消息</span>
-			<span></span>
+			<span>&#xe8c0;</span>
 		</header>	
 	</div>
 </template>
@@ -14,14 +14,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/style/usage/core/reset.scss';
+@font-face {
+  font-family: 'yofont';
+  src:
+    url('../../assets/iconfonts/newsicon/iconfont.woff') format('woff'),
+    url('../../assets/iconfonts/newsicon/iconfont.ttf') format('truetype')
+}
 	header{
 		width:100%;
 		height:0.44rem;
 		background:#fff;
-		@include border()
+		text-align:center;
+		@include border(0 0 1px 0,#ececec,solid)
 		span:nth-child(1){
-			text-align:center;
 			line-height:0.44rem;
+			font-size:.16rem;
+			color:#333;
+			font-weight:500;
 		}
+		span:nth-child(2){
+				width:.22rem;
+				height:.22rem;
+				font-family:yofont;
+				display:inline-block;
+				position: absolute;
+		    top: .11rem;
+		    right: .2rem;
+		    font-size:.18rem;
+		    line-height:.22rem;
+		    color:#999;
+			}
 	}
 </style>
